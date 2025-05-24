01dd4a2bd3cc905ab89f3637994694b48d6baf07
unknown <mehedytouhidurzaman@gmail.com>
2025-05-25 05:25:47 +0600

    The problem 3 is completed

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -42,12 +42,14 @@ VALUES (1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image capture
 (3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
 (4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
-
-
-
 -- Problem-01
 INSERT INTO rangers(ranger_id,name, region)
 VALUES(4,'Derek Fox', 'Coastal Plains');
 
 -- Problem-02
 SELECT count(DISTINCT species_id) as unique_species_count FROM sightings;
+
+-- Problem-03
+SELECT * from sightings
+    WHERE location LIKE '%Pass%';
+
